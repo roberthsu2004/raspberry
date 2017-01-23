dd38e88837b4063b01adbd77faf52d41bddae13d
robert <roberthsu2003@gmail.com>
2017-01-23 13:46:55 +0800

    pwmled.py modify

--- Firebase_GPIO_tkinter/2PWMLed/pwmLed.py
+++ Firebase_GPIO_tkinter/2PWMLed/pwmLed.py
@@ -3,12 +3,19 @@ from time import sleep
 from tkinter import *
 import requests
 import json
+import threading
 
 
 class App:
-    firebase_url = "https://raspberryfirebase.firebaseio.com/";
+    firebase_url = "https://raspberryfirebase.firebaseio.com";
     __job = None;
+    __currentLedValue=0;
+    __pin = 25;
     def __init__(self,master):
+        '''initital led'''
+        self.led = PWMLED(self.__pin);
+        self.led.value = 0;
+        '''initial tkinter'''
         self.master = master;
         mainFrame = Frame(master);
         subFrame = Frame(mainFrame,relief=GROOVE,borderwidth=2);
@@ -19,6 +26,7 @@ class App:
         self.scale.pack(fill=X,expand=YES,padx=20);        
         subFrame.pack(fill=BOTH,expand=YES,padx=20,pady=20);
         mainFrame.pack(fill=BOTH,expand=YES);
+        self.getDataFromFirebase();
 
     def userUpdateValue(self,event):
         if self.__job:
@@ -30,6 +38,20 @@ class App:
         self.__jon = None;
         getData = {"pwm_value":self.scale.get()};
         requests.put(self.firebase_url + "/" + "raspberrypi/PWM_Led.json",data=json.dumps(getData));
+
+    def getDataFromFirebase(self):        
+        r = requests.get(self.firebase_url + "/raspberrypi/PWM_Led.json");
+        try:
+            receiveData = r.json();
+            value = receiveData["pwm_value"];
+            if  value != self.__currentLedValue:
+                self.__currentLedValue = value;
+                self.led.value = value/100;
+               
+        except:
+            return;
+        
+        threading.Timer(1,self.getDataFromFirebase).start();
         
         
 
