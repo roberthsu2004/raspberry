e64cb30eadfbd98ed6c3f7a167a7e23284ecd761
Robert Hsu <roberthsu2003@gmail.com>
2017-07-25 16:24:38 +0800

    c

--- file.py
+++ file.py
@@ -9,3 +9,19 @@ print(b);
 print(c);
 print(d);
 print(e);
+
+>>> x = input("Enter Value:")
+Enter Value:23
+>>> print(x)
+23
+>>> tempc = input("Enter temp in C:")
+Enter temp in C:20
+>>> tempF = (int(tempC) * 9) / 5 + 32
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    tempF = (int(tempC) * 9) / 5 + 32
+NameError: name 'tempC' is not defined
+>>> tempF = (int(tempc) * 9) /5 + 32
+>>> print(tempF);
+68.0
+>>> 
