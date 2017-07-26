41bc24ca10e62ac0a7f9f88bc7b1358eeabae0ec
Robert Hsu <roberthsu2003@gmail.com>
2017-07-26 15:46:32 +0800

    m

--- file.py
+++ file.py
@@ -1,27 +1,12 @@
->>> int("123") + 123
-246
->>> str(123)
-'123'
->>> int("-123")
--123
->>> float("00123.45");
-123.45
->>> int("1001",2)
-9
->>> int("AFF0", 16)
-45040
->>> len("abcdef")
-6
->>> s = "abcdefghi";
->>> s.find("def")
-3
->>> s = "abcdefghi"
->>> s[1:5]
-'bcde'
->>> s[0:5]
-'abcde'
->>> s[:5]
-'abcde'
->>> s[3:]
-'defghi'
->>> 
+from gpiozero import LED
+from time import sleep
+
+led = LED(4);
+
+while True:
+    led.on();
+    sleep(1);
+    print("on");
+    led.off();
+    sleep(1);
+    print("off");
