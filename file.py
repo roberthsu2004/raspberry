c71a81d2d986bcb2e1d23e84e51a062d80fd98f7
Robert Hsu <roberthsu2003@gmail.com>
2017-07-26 13:53:45 +0800

    c

--- file.py
+++ file.py
@@ -1,27 +1,26 @@
-a = 123
-b = 12.34
-c = "hello"
-d = 'Hello'
-e = True
+Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
+[GCC 4.9.1] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> for i in range(1,10,1):
+	print(i);
 
-print(a);
-print(b);
-print(c);
-print(d);
-print(e);
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> for i in range(1,10,2):
+	print(i);
 
->>> x = input("Enter Value:")
-Enter Value:23
->>> print(x)
-23
->>> tempc = input("Enter temp in C:")
-Enter temp in C:20
->>> tempF = (int(tempC) * 9) / 5 + 32
-Traceback (most recent call last):
-  File "<pyshell#4>", line 1, in <module>
-    tempF = (int(tempC) * 9) / 5 + 32
-NameError: name 'tempC' is not defined
->>> tempF = (int(tempc) * 9) /5 + 32
->>> print(tempF);
-68.0
+	
+1
+3
+5
+7
+9
 >>> 
