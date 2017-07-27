753d1e4f2934e94d2b3638d2c1d6bff888016f3e
Robert Hsu <roberthsu2003@gmail.com>
2017-07-27 14:30:46 +0800

    m

--- file.py
+++ file.py
@@ -1,9 +1,30 @@
->>> from gpiozero import LED
->>> led = LED(26)
->>> led.on()
->>> led.off()
->>> led.toggle();
->>> led.toggle();
->>> led.blink(1,1,3,True)
->>> led.blink(0.3,0.3,100,True)
+Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
+[GCC 4.9.1] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 101
+>>> if x > 100:
+	print("x is big")
+
+	
+x is big
+>>> x = 101
+>>> if x > 100:
+	print("x is big")
+else:
+	print("x is small")
+
+x is big
+>>> if x > 100:
+	print("x is big")
+else:
+	print("x is small")
+print();
+SyntaxError: invalid syntax
+>>> 
+>>> if x > 100:
+	print("x is big")
+else:
+	print("x is small")
+print("this will always print");
+SyntaxError: invalid syntax
 >>> 
