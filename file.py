3976a7e0e0cf629c30ef63f91bc6eaaff60b8904
Robert Hsu <roberthsu2003@gmail.com>
2017-07-27 14:38:47 +0800

    m

--- file.py
+++ file.py
@@ -1,30 +1,15 @@
-Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
-[GCC 4.9.1] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> x = 101
->>> if x > 100:
-	print("x is big")
+x = 101;
+if x > 100:
+    print("x is big");
+else:
+    print("x is small");
+print("This will always print");
 
-	
-x is big
->>> x = 101
->>> if x > 100:
-	print("x is big")
-else:
-	print("x is small")
 
-x is big
->>> if x > 100:
-	print("x is big")
+x = 90;
+if x > 100:
+    print("x is big");
+elif x < 10:
+    print("x is small");
 else:
-	print("x is small")
-print();
-SyntaxError: invalid syntax
->>> 
->>> if x > 100:
-	print("x is big")
-else:
-	print("x is small")
-print("this will always print");
-SyntaxError: invalid syntax
->>> 
+    print("x is medium");
