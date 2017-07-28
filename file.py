b301dfce6671d80f4e25d59df2fedac8bfa37be0
Robert Hsu <roberthsu2003@gmail.com>
2017-07-28 14:13:31 +0800

    m

--- file.py
+++ file.py
@@ -1,1 +1,11 @@
-sudo apt-get install python3-requests
+from gpiozero import Button
+from time import sleep
+
+button = Button(pin=2,pull_up=True,bounce_time=None);
+while True:
+    if button.is_pressed:
+        print("button is pressed");
+    else:
+        print("button is not pressed");
+
+    sleep(1);
