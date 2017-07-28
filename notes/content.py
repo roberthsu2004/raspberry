66fc4d2f2832414db3338825ea8eb937d19c70a5
Robert Hsu <roberthsu2003@gmail.com>
2017-07-28 16:28:06 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,4 +1,11 @@
+from gpiozero import LED
 import requests
 content = requests.get("https://raspberryfirebase.firebaseio.com/test/lcd.json", verify=False)
 print("status code:",content.status_code);
 print("json:",content.json());
+
+led = LED(17);
+if content.json():
+    led.on();
+else:
+    led.off();
