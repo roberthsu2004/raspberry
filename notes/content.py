f0d98ca170807d00d485629aa312f35def751b09
Robert Hsu <roberthsu2003@gmail.com>
2017-07-29 20:01:15 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,1 +1,15 @@
-https://raspberryfirebase.firebaseio.com/
+import requests
+from time import sleep
+from gpiozero import LED
+
+led = LED(26);
+while True:
+    firebase_url = "https://raspberryfirebase.firebaseio.com/"
+    r = requests.get(firebase_url+"/led.json?print=pretty",verify=False);
+    
+    if r.json():   
+        led.on();
+    else:
+        led.off();
+    
+    sleep(1);
