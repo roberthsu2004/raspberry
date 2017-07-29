c7377d6221a6f58da0b97c4fc2f15cdd9c64cad6
Robert Hsu <roberthsu2003@gmail.com>
2017-07-29 18:58:49 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,59 +1,27 @@
-Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
-[GCC 4.9.1] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> <
-SyntaxError: invalid syntax
->>> >
-SyntaxError: invalid syntax
->>> <=
-SyntaxError: invalid syntax
->>> >=
-SyntaxError: invalid syntax
->>> ==
-SyntaxError: invalid syntax
->>> !=
-SyntaxError: invalid syntax
->>> 1 != 2
-True
->>> 1 != 1
-False
->>> x = 17
->>> if x >= 10 and x <=20:
-	print("x is in the middle");
+def count_to_10():
+    for i in range(1, 11):
+        print(i);
 
-	
-x is in the middle
->>> for i in range(1,11):
-	print(i);
+count_to_10();
 
-	
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
->>> answer = ''
->>> while answer != 'X':
-	answer = input("enter command:");
+def count_to_n(n):
+    for i in range(1, n+1):
+        print(i);
 
-	
-enter command:5
-enter command:6
-enter command:X
->>> while True:
-	answer = input('Enter command:');
-	if answer == 'X':
-		break;
+count_to_n(5);
 
-	
-Enter command:4
-Enter command:6
-Enter command:9
-Enter command:X
->>> 
->>> 
+def count_to_n(n=10):
+    for i in range(1, n+1):
+        print(i);
+
+count_to_n();
+
+
+def count(from_num=1, to_num=10):
+    for i in range(from_num, to_num + 1):
+        print(i);
+
+count();
+count(5);
+count(5,10);
+    
