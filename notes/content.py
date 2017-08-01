d8c2518e0338cbd1bc3edbca80a48423a4b95f8d
Robert Hsu <roberthsu2003@gmail.com>
2017-08-01 14:45:16 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -5,13 +5,12 @@ import json
 
 firebase_url = "https://raspberryfirebase.firebaseio.com/"
 
-def button_press():
-    print("button press");
-    content = requests.get(firebase_url + "test/led.json",verify=False);
+def button_press():        
+    content = requests.get(firebase_url + "test/led.json",verify=False);    
     newState = not content.json();    
     jsonValue = {"led":newState}
     requests.put(firebase_url + "test.json",data=json.dumps(jsonValue));
+    
 
 button = Button(17);
 button.when_pressed = button_press;
-pause();
