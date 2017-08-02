908c76cdc25795ab0810cf4cd484f39ca46fcf2b
Robert Hsu <roberthsu2003@gmail.com>
2017-08-02 14:05:35 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,16 +1,1 @@
-from gpiozero import Button
-from signal import pause
-import requests
-import json
-
-firebase_url = "https://raspberryfirebase.firebaseio.com/"
-
-def button_press():        
-    content = requests.get(firebase_url + "test/led.json",verify=False);    
-    newState = not content.json();    
-    jsonValue = {"led":newState}
-    requests.put(firebase_url + "test.json",data=json.dumps(jsonValue));
-    
-
-button = Button(17);
-button.when_pressed = button_press;
+https://sites.google.com/site/raspberrypidiy/pc-to-rpi/vsftpd
