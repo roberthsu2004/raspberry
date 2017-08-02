1c1d26c593c9c35e59ebdf13df1b61e7bada5609
Robert Hsu <roberthsu2003@gmail.com>
2017-08-02 14:46:00 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,3 +1,13 @@
 from datetime import datetime
 d = datetime.now();
 print("{:%Y-%m-%d %H:%M:%S}".format(d));
+
+
+def calculate_temperatures(kelvin):
+    celsius = kelvin - 273;
+    fahrenheit = celsius * 9 / 5 + 32;
+    return (celsius, fahrenheit)
+
+(c,f) = calculate_temperatures(340);
+print("celsius:",c);
+print("fahrenheit",f);
