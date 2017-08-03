b7dba84af74f92482aa54bbb3c813be4c76311c0
Robert Hsu <roberthsu2003@gmail.com>
2017-08-03 15:47:36 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,23 +1,7 @@
-{"raspberrypi":{
-  "LED_Control" : {
-    "LED25" : "OPEN",
-    "date" : "2017"
-  },
-  "MCP3008" : {
-    "brightness" : "0.20",
-    "temperature" : "24.19"
-  },
-  "PWM_Led" : {
-    "pwm_value" : 42
-  },
-  "RGB_LED" : {
-    "blue" : 0,
-    "green" : 0,
-    "red" : 0
-  },
-  "servo" : {
-    "angle" : 75,
-    "distance" : 180
-  }
-}
-}
+from tkinter import *
+
+root = Tk();
+root.title('Heloo!World');
+root.geometry("400x200");
+
+root.mainloop();
