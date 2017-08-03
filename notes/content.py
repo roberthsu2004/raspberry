f127eaa4aed45d0e8053d73779eb68a6a6334ad4
Robert Hsu <roberthsu2003@gmail.com>
2017-08-03 16:29:08 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -3,6 +3,12 @@ from tkinter import *
 class App:
     def __init__(self,master):
         master.title("LED Control");
+        master.option_add("*Font",("Verdana",18,"bold"));
+        self.ledText = StringVar();
+        f = Frame(master);
+        xf = Frame(f,relief=GROOVE,borderwidth=2);
+        xf.pack(fill=BOTH,expand=YES,padx=20,pady=20);
+        f.pack(fill=BOTH,expand=YES);
 
 root = Tk();
 root.geometry("400x300");
