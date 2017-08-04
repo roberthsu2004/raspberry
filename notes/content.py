f69a2790fc41ddb1d6def56666bc1b24ce8f5c41
Robert Hsu <roberthsu2003@gmail.com>
2017-08-04 15:11:52 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,6 +1,14 @@
 from tkinter import *
+import requests
+import json
+from gpiozero import LED
+import threading
 
 class App:
+    led = LED(25);
+    ledState = "";
+    firebase_url = "https://raspberryfirebase.firebaseio.com/"
+    
     def __init__(self,master):
         master.title("LED Control");
         master.option_add("*Font",("Verdana",18,"bold"));
@@ -12,9 +20,23 @@ class App:
         self.ledText.set("LED OPEN");
         xf.pack(fill=BOTH,expand=20, padx=20,pady=20);
         f.pack(fill=BOTH,expand=YES);
+        self.getFirebaseData();
     
     def userClick(self):
         print("userClick");
+
+    def getFirebaseData(self):
+        r = requests.get(self.firebase_url + "raspberrypi/LED_Control.json");
+        if r.status_code == 200 :
+            self.ledState = r.json()["LED25"];
+            if self.ledState == "OPEN":
+                self.led.on();
+                self.ledText.set("LED CLOSE");
+            elif self.ledState == "CLOSE":
+                self.led.off();
+                self.ledText.set("LED OPEN");
+        threading.Timer(0.1,self.getFirebaseData).start();
+                
 
 root = Tk();
 root.geometry("400x300");
