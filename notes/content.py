467ef7903286bd8dbb99ca02b7c4e521e86254e9
Robert Hsu <roberthsu2003@gmail.com>
2017-08-04 14:45:52 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -8,8 +8,13 @@ class App:
         f = Frame(master);
         xf = Frame(f,relief=GROOVE,borderwidth=2);
         titleLabel = Label(f,text="LED control").place(relx=0.05,rely=0.025,anchor=NW);
+        button = Button(xf,textvariable=self.ledText,command=lambda:self.userClick()).pack(expand=YES,fill=BOTH,padx=50,pady=50);
+        self.ledText.set("LED OPEN");
         xf.pack(fill=BOTH,expand=20, padx=20,pady=20);
         f.pack(fill=BOTH,expand=YES);
+    
+    def userClick(self):
+        print("userClick");
 
 root = Tk();
 root.geometry("400x300");
