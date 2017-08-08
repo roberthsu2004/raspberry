400811d7afb22456be70dfa8e7069a9fe5b7acd9
Robert Hsu <roberthsu2003@gmail.com>
2017-08-08 14:16:17 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,11 +1,18 @@
 from tkinter import *
 import requests
 import json
+import threading
+from gpiozero import PWMLED
 
 class App:
     firebase_url = "https://raspberryfirebase.firebaseio.com"
     __job = None;
+    __pin = 25;
     def __init__(self,master):
+        '''initial gpio'''
+        self.led = PWMLED(self.__pin);
+        self.led.value = 0;
+        '''initial layout'''
         self.master = master;
         mainFrame = Frame(master);
         subFrame = Frame(mainFrame,relief=GROOVE,borderwidth=2);
@@ -16,6 +23,7 @@ class App:
         self.scale.pack(fill=X,expand=YES,padx=20);
         subFrame.pack(fill=BOTH,expand=YES,padx=20,pady=20);
         mainFrame.pack(fill=BOTH,expand=YES);
+        self.getDataFromFirebase();
 
     def userUpdateValue(self,event):
        print("user update");
@@ -29,6 +37,20 @@ class App:
         requests.put(self.firebase_url + "/raspberrypi/PWM_Led.json",data=json.dumps(getData));
         
         self.__job = None;
+
+    def getDataFromFirebase(self):
+        response = requests.get(self.firebase_url + "/raspberrypi/PWM_Led.json");
+        try:
+            receiveData = response.json();
+            value = receiveData["pwm_value"];
+            self.led.value = value/100;
+            self.scaleValue.set(value);
+        except:
+            print("run except");
+            threading.Timer(0.2,self.getDataFromFirebase).start();
+            return;
+
+        threading.Timer(0.2,self.getDataFromFirebase).start();
 
 root = Tk();
 root.title("PWMLED");
