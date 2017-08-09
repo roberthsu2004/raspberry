6d42422139ea884bca8aeab89805611e906ccffb
Robert Hsu <roberthsu2003@gmail.com>
2017-08-09 15:25:56 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,5 +1,17 @@
-from gpiozero import LED
+from gpiozero import MCP3008,LED
+from time import sleep
 
+pot = MCP3008(0);
+ldr = MCP3008(1);
 relay = LED(17);
-relay.on();
-relay.off();
+
+while True:
+    print("variable:%.5f" % pot.value);
+    print("======================");
+    print("light:%.5f" % ldr.value);
+    #light = 0.11 , dark = 0.02
+    if ldr.value > 0.06:
+        relay.on();
+    else:
+        relay.off();
+    sleep(1);
