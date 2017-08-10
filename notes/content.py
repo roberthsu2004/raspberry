c2f194d4cbe8913f850bba6adb02f7060f7fdbbb
Robert Hsu <roberthsu2003@gmail.com>
2017-08-10 15:56:21 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,9 +1,12 @@
 from HR04 import HR04
+from gpiozero import AngularServo
 from time import sleep
 
 hr04 = HR04(23,24);
-
+servo = AngularServo(18,min_angle=0,max_angle=90);
+servo.angle = 90;
 while True:
+    
     try:
         distance = hr04.getCmDistance();
         print("Distance to %.2f cm" % distance);
