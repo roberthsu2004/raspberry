66bb234db316a067091d685b2ad212204fb3e235
Robert Hsu <roberthsu2003@gmail.com>
2017-08-10 16:20:03 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -4,13 +4,19 @@ from time import sleep
 
 hr04 = HR04(23,24);
 servo = AngularServo(18,min_angle=0,max_angle=90);
-servo.angle = 90;
+
 while True:
-    
+    '''
     try:
         distance = hr04.getCmDistance();
         print("Distance to %.2f cm" % distance);
+       
     except:
         print("too long");
         pass;
+    '''
+    
+    servo.min();
+    sleep(1);
+    servo.max();
     sleep(1);
