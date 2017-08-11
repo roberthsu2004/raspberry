dfc11107b2db29623841b5f40fa9e6d45648c819
Robert Hsu <roberthsu2003@gmail.com>
2017-08-11 15:00:32 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -3,3 +3,16 @@ from gpiozero import AngularServo
 from HR04 import HR04
 import requests
 import json
+
+class App:
+    def __init__(self,master):
+        self.master = master;
+
+if __name__ == "__main__":
+    root = Tk();
+    root.title("servo And HC-HR04");
+    root.option_add("*font",("Helvetica",18,"bold"));
+    root.option_add("*background","gold");
+    display = App(root);
+    root.mainloop();
+    
