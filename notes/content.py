7300a23856cb2bdf87f4ce049a492e78709e38dd
Robert Hsu <roberthsu2003@gmail.com>
2017-08-11 15:26:12 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -4,9 +4,23 @@ from HR04 import HR04
 import requests
 import json
 
+
 class App:
     def __init__(self,master):
         self.master = master;
+        #HR04
+        self.hr04 = HR04(23,24);
+        self.distanceHandler();
+        
+        
+    def distanceHandler(self):
+        distance = self.hr04.getCmDistance();
+        if distance != None:
+            print("%.2f cm" % distance);
+        else:
+            print("too long");
+            
+        self.master.after(1000,self.distanceHandler);
 
 if __name__ == "__main__":
     root = Tk();
