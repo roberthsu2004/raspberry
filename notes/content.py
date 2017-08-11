bfcbf828a82f5d2cb14abb434fc03d2144b54280
Robert Hsu <roberthsu2003@gmail.com>
2017-08-11 16:13:58 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -8,7 +8,9 @@ import json
 class App:
     def __init__(self,master):
         self.master = master;       
-
+        self.distanceValue = 0;
+        self.valueVariable = IntVar();
+        
         #for gpio servo initial
         self.servo = AngularServo(18,min_angle=0,max_angle=90);
         self.servo.angle = 0;
@@ -21,9 +23,20 @@ class App:
         mainFrame = Frame(self.master);
         servoFrame = Frame(mainFrame);
         borderFrame = Frame(servoFrame,borderwidth=2,relief=GROOVE,pady=30,padx=10);
+        Label(servoFrame,text="Servo").place(relx=0.03,anchor=NW);
+
+        self.button0 = Radiobutton(borderFrame,text="0",variable=self.valueVariable,value=0,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+        self.button25 = Radiobutton(borderFrame,text="25",variable=self.valueVariable,value=25,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+        self.button50 = Radiobutton(borderFrame,text="50",variable=self.valueVariable,value=50,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+        self.button75 = Radiobutton(borderFrame,text="75",variable=self.valueVariable,value=75,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+        self.button90 = Radiobutton(borderFrame,text="90",variable=self.valueVariable,value=90,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
         borderFrame.pack(side=TOP,padx=10,pady=10);
         servoFrame.pack(side=TOP);
         mainFrame.pack();
+        
+    def changeDegree(self):
+        self.angleValue = self.valueVariable.get();
+        print(self.angleValue);
         
         
     def distanceHandler(self):
