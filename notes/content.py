be72cbe8878eff6aad5a5eb9a0e11aab8f2b140a
Robert Hsu <roberthsu2003@gmail.com>
2017-08-11 14:45:39 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,28 +1,5 @@
-from HR04 import HR04
+from tkinter import *
 from gpiozero import AngularServo
-from time import sleep
-
-hr04 = HR04(23,24);
-servo = AngularServo(18,min_angle=0,max_angle=90);
-
-while True:
-        try:
-                distance = hr04.getCmDistance();
-                print("Distance to %.2f cm" % distance);
-                
-                if distance < 10:
-                    servo.min();
-                else:
-                    servo.max();
-        except:
-                print("too long");
-                pass;
-        
-        sleep(1);
-
-        
-
-    
-    
-    
-    
+from HR04 import HR04
+import requests
+import json
