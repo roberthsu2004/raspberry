0745890f0ca250544e4f04e45eea598e0850461b
Robert Hsu <roberthsu2003@gmail.com>
2017-08-11 13:59:57 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -6,17 +6,23 @@ hr04 = HR04(23,24);
 servo = AngularServo(18,min_angle=0,max_angle=90);
 
 while True:
-    '''
-    try:
-        distance = hr04.getCmDistance();
-        print("Distance to %.2f cm" % distance);
-       
-    except:
-        print("too long");
-        pass;
-    '''
+        try:
+                distance = hr04.getCmDistance();
+                print("Distance to %.2f cm" % distance);
+                
+                if distance < 10:
+                    servo.min();
+                else:
+                    servo.max();
+        except:
+                print("too long");
+                pass;
+        
+        sleep(1);
+
+        
+
     
-    servo.min();
-    sleep(1);
-    servo.max();
-    sleep(1);
+    
+    
+    
