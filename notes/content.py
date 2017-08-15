f32bea484cb8ed0229a1aeb625549ba3d6aa20c1
Robert Hsu <roberthsu2003@gmail.com>
2017-08-15 16:02:59 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -2,8 +2,18 @@ from gpiozero import MCP3008
 from tkinter import *
 
 class App:
+    ldr = MCP3008(channel=7);
+    adc = MCP3008(channel=6);
     def __init__(self,master):
         self.master = master;
+        self.autoUpdate();
+
+    def autoUpdate(self):
+        print("adc.value:%.2f" % (App.adc.value * 3.3 * 100));
+        print("ldr.value:%.2f" % App.ldr.value);
+        
+        self.master.after(500,self.autoUpdate);
+        
 
 root = Tk();
 root.title("brightness");
