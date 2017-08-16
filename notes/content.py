458c48532fd3dd3b191951c1a195a7b6d88df528
Robert Hsu <roberthsu2003@gmail.com>
2017-08-16 15:33:52 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,11 +1,14 @@
 from tkinter import *
 class App:
         def __init__(self,master):
-            master.geometry("300x200");
+            #master.geometry("300x200");
             fm = Frame(master);
-            Button(fm, text="Top").pack(side=TOP,expand=YES);
-            Button(fm, text="Center").pack(side=TOP,expand=YES);
-            Button(fm, text="Bottom").pack(side=TOP,expand=YES);
+            Button(fm, text="Top").pack(side=TOP,expand=YES,fill=BOTH);
+            Button(fm, text="Center").pack(side=TOP,expand=YES,fill=BOTH);
+            Button(fm, text="Bottom").pack(side=TOP,expand=YES,fill=BOTH);
+            Button(fm, text="Left").pack(side=LEFT);
+            Button(fm, text="This is Center").pack(side=LEFT);
+            Button(fm, text="Right").pack(side=LEFT);
             fm.pack(fill=BOTH,expand=YES);
 root = Tk();
 root.option_add("*font",("verdana",12,"bold"));
