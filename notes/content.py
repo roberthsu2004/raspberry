61d5a7fd4be81d36923feb0a4668a2074e5a5a1c
Robert Hsu <roberthsu2003@gmail.com>
2017-08-17 13:45:29 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -3,25 +3,34 @@ class App:
         def __init__(self,master):
             master.geometry("800x800");
             fm1 = Frame(master);
-            Button(fm1, text="button1").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm1, text="button2").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm1, text="button3").pack(side=LEFT,expand=YES,fill=BOTH);
+            fm1Border = Frame(fm1,borderwidth=2,relief=GROOVE);
+            fm1Title = Label(fm1,text="停車庫").place(relx=0.03,rely=0.08,anchor=NW);
+            Button(fm1Border, text="button1").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(fm1Border, text="button2").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(fm1Border, text="button3").pack(side=LEFT,expand=YES,fill=BOTH);
+            fm1Border.pack(side=LEFT,expand=YES,fill=BOTH,padx=10,pady=10);
             fm1.pack(side=TOP,expand=YES,fill=BOTH);
             
             fm2 = Frame(master);
-            Button(fm2, text="button4").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm2, text="button5").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm2, text="button6").pack(side=LEFT,expand=YES,fill=BOTH);
+            fm2Border = Frame(fm2,borderwidth=2,relief=GROOVE);
+            fm2Title = Label(fm2,text="一樓客廳").place(relx=0.03,rely=0.08,anchor=NW);
+            Button(fm2Border, text="button4").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(fm2Border, text="button5").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(fm2Border, text="button6").pack(side=LEFT,expand=YES,fill=BOTH);
+            fm2Border.pack(side=LEFT,expand=YES,fill=BOTH,padx=10,pady=10);
             fm2.pack(side=TOP,expand=YES,fill=BOTH);
             
             fm3 = Frame(master);
-            Button(fm3, text="button7").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm3, text="button8").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm3, text="button9").pack(side=LEFT,expand=YES,fill=BOTH);
+            fm3Border = Frame(fm3,borderwidth=2,relief=GROOVE);
+            fm3Title = Label(fm3,text="二摟臥室").place(relx=0.03,rely=0.08,anchor=NW);
+            Button(fm3Border, text="button7").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(fm3Border, text="button8").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(fm3Border, text="button9").pack(side=LEFT,expand=YES,fill=BOTH);
+            fm3Border.pack(side=LEFT,expand=YES,fill=BOTH,padx=10,pady=10);
             fm3.pack(side=TOP,expand=YES,fill=BOTH);
             
 root = Tk();
-root.option_add("*font",("verdana",12,"bold"));
-root.title("Pack - Example 7");
+root.option_add("*font",("verdana",18));
+root.title("Home");
 display = App(root)
 root.mainloop();
