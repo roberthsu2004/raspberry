d86a00f421bea63e9afcc18580bb021bb9990ab6
Robert Hsu <roberthsu2003@gmail.com>
2017-08-17 14:22:39 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -4,30 +4,39 @@ class App:
             master.geometry("800x800");
             fm1 = Frame(master);
             fm1Border = Frame(fm1,borderwidth=2,relief=GROOVE);
-            fm1Title = Label(fm1Border,text="停車庫").pack(side=TOP,pady=10,padx=10,anchor=W);
-            Button(fm1Border, text="button1").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm1Border, text="button2").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm1Border, text="button3").pack(side=LEFT,expand=YES,fill=BOTH);
+            self.__garageLayout(fm1Border);
             fm1Border.pack(side=LEFT,expand=YES,fill=BOTH,padx=10,pady=10);
             fm1.pack(side=TOP,expand=YES,fill=BOTH);
-            
+        
             fm2 = Frame(master);
             fm2Border = Frame(fm2,borderwidth=2,relief=GROOVE);
-            fm2Title = Label(fm2Border,text="一樓客廳").pack(side=TOP,pady=10,padx=10,anchor=W);
-            Button(fm2Border, text="button4").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm2Border, text="button5").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm2Border, text="button6").pack(side=LEFT,expand=YES,fill=BOTH);
+            self.__livingRoomLayout(fm2Border);
             fm2Border.pack(side=LEFT,expand=YES,fill=BOTH,padx=10,pady=10);
             fm2.pack(side=TOP,expand=YES,fill=BOTH);
             
             fm3 = Frame(master);
             fm3Border = Frame(fm3,borderwidth=2,relief=GROOVE);
-            fm3Title = Label(fm3Border,text="二摟臥室").pack(side=TOP,pady=10,padx=10,anchor=W);
-            Button(fm3Border, text="button7").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm3Border, text="button8").pack(side=LEFT,expand=YES,fill=BOTH);
-            Button(fm3Border, text="button9").pack(side=LEFT,expand=YES,fill=BOTH);
+            self.__bathRoomLayout(fm3Border);
             fm3Border.pack(side=LEFT,expand=YES,fill=BOTH,padx=10,pady=10);
             fm3.pack(side=TOP,expand=YES,fill=BOTH);
+        
+        def __garageLayout(self,frame):
+            fm1Title = Label(frame,text="停車庫").pack(side=TOP,pady=10,padx=10,anchor=W);
+            Button(frame, text="button1").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(frame, text="button2").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(frame, text="button3").pack(side=LEFT,expand=YES,fill=BOTH);
+
+        def __livingRoomLayout(self,frame):
+            fm2Title = Label(frame,text="一樓客廳").pack(side=TOP,pady=10,padx=10,anchor=W);
+            Button(frame, text="button4").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(frame, text="button5").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(frame, text="button6").pack(side=LEFT,expand=YES,fill=BOTH);
+
+        def __bathRoomLayout(self,frame):
+            fm3Title = Label(frame,text="二摟臥室").pack(side=TOP,pady=10,padx=10,anchor=W);
+            Button(frame, text="button7").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(frame, text="button8").pack(side=LEFT,expand=YES,fill=BOTH);
+            Button(frame, text="button9").pack(side=LEFT,expand=YES,fill=BOTH);    
             
 root = Tk();
 root.option_add("*font",("verdana",18));
