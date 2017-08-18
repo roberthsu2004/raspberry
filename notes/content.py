e271cfa59663e703297862258cffd074e4d419d3
Robert Hsu <roberthsu2003@gmail.com>
2017-08-18 14:52:17 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,13 +1,13 @@
-from gpiozero import MotionSensor
-
-def main():
-    motionSensor = MotionSensor(pin=21,queue_len=5,sample_rate=120,threshold=0.2);
-    
-    while True:
-        if motionSensor.motion_detected:
-            print("active");
-        else:
-            print("inactive");
-            
-if __name__ == "__main__":
-    main();
+import RPi.GPIO as GPIO                           #Import GPIO library
+import time                                       #Import time library
+GPIO.setmode(GPIO.BOARD)                          #Set GPIO pin numbering
+pir = 26                                          #Associate pin 26 to pir
+GPIO.setup(pir, GPIO.IN)                          #Set pin as GPIO in 
+print "Waiting for sensor to settle"
+time.sleep(2)                                     #Waiting 2 seconds for the sensor to initiate
+print "Detecting motion"
+while True:
+   if GPIO.input(pir):                            #Check whether pir is HIGH
+      print "Motion Detected!"
+      time.sleep(2)                               #D1- Delay to avoid multiple detection
+   time.sleep(0.1)                                #While loop delay should be less than detection(hardware) delay
