3d097fe03ad5d572e0cfa2a30c8494cac450516b
Robert Hsu <roberthsu2003@gmail.com>
2017-08-18 14:23:18 +0800

    m

--- notes/content.py
+++ notes/content.py
@@ -1,7 +1,7 @@
 from gpiozero import MotionSensor
 
 def main():
-    motionSensor = MotionSensor(21);
+    motionSensor = MotionSensor(pin=21,queue_len=5,sample_rate=120,threshold=0.2);
     
     while True:
         if motionSensor.motion_detected:
