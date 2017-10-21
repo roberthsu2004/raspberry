dc30b1955229533c1b14c14ef12a28f475182836
Robert Hsu <roberthsu2003@gmail.com>
2017-10-21 16:09:47 +0800

    更新

--- Firebase_GPIO_tkinter/1LEDControl/a1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/a1_LEDControl.py
@@ -1,68 +1,66 @@
 from tkinter import *
 from gpiozero import LED
+import requests
+import threading
 import datetime
 import time
-import requests
 import json
-import threading
-
 
 class App:
-    led = LED(25);
-    led1= LED(17);
-    ledState = "";
     firebase_url = "https://raspberryfirebase.firebaseio.com/";
     def __init__(self,master):
+        self.ledText = StringVar();
+        self.ledState = "";
+        self.led25 = LED(25);
         master.title("LED Control");
         master.option_add("*Font",("verdana",18,"bold"));
-        self.ledText = StringVar();
         f = Frame(master);
         xf = Frame(f,relief=GROOVE,borderwidth=2);
         titleLabel = Label(f,text="LED Control").place(relx=0.05,rely=0.025,anchor=NW);
-        button = Button(xf,textvariable=self.ledText,command=lambda: self.userClick()).pack(fill=BOTH,expand=YES,padx=50,pady=80);
-        self.ledText.set("LED OPEN");
-        xf.pack(fill=BOTH,expand=YES,padx=5,pady=20);
+        xf.pack(expand=YES,fill=BOTH,padx=5,pady=20);
+        button = Button(xf,textvariable=self.ledText,command=lambda :self.userClick()).pack(expand=YES,fill=BOTH,padx=80,pady=50);
         f.pack(fill=BOTH,expand=YES);
         self.getFirebaseData();
-
-        
-
+    
     def userClick(self):
-                
-        if self.led.is_lit == True :
-            self.ledState = "CLOSE"
+        if self.ledState == "OPEN" :
+            newState = "CLOSE";
         else:
-            self.ledState = "OPEN";
+            newState = "OPEN";
         
-
         t = time.time();
         date = datetime.datetime.fromtimestamp(t).strftime("%Y-%m-%d-%H-%M-%S");
-        data = {'date':date,"LED25":self.ledState};
-        result = requests.put(self.firebase_url + "/" + "raspberrypi/LED_Control.json",data=json.dumps(data));
-        print("status code = %s, Response =%s" % (str(result.status_code),result.text));
+        data = {"LED25":newState,"date":date};
+        try:
+            result = requests.put(App.firebase_url + "/" + "raspberrypi/LED_Control.json",data=json.dumps(data));
+        except:
+            return;
+        print("status code = %s, Respone = %s" % (str(result.status_code),result.text));
 
-    def getFirebaseData(self):                
-        r = requests.get(self.firebase_url + "/" + "raspberrypi/LED_Control.json");
-        if r.status_code == 200:            
-            self.ledState = r.json()["LED25"];
+        
+    
+    def getFirebaseData(self):
+        try:
+            response = requests.get(App.firebase_url + "/" + "raspberrypi/LED_Control.json");
+        except:
+            threading.Timer(0.1,self.getFirebaseData).start();
+            return;
+        
+        if response.status_code == 200 :
+            self.ledState = response.json()["LED25"];
             if self.ledState == "OPEN":
-                    self.led.on();
-                    self.ledText.set("LED CLOSE");
+                self.ledText.set("LED CLOSE");
+                self.led25.on();
             elif self.ledState == "CLOSE":
-                    print("status:close");
-                    self.led.off();
-                    self.ledText.set("LED OPEN");
+                self.ledText.set("LED OPEN");
+                self.led25.off();
+        
         threading.Timer(0.1,self.getFirebaseData).start();
-                    
-                    
 
-        
-        
-        
+
+
 
 root = Tk();
 root.geometry("400x300");
-display  = App(root);
-
+display = App(root);
 root.mainloop();
-
