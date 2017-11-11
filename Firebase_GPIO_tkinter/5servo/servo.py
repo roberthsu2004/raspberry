f75eb3a91c989f97fb9e5b8bff59300fdc9a722b
Robert Hsu <roberthsu2003@gmail.com>
2017-11-11 15:46:14 +0800

    modify

--- Firebase_GPIO_tkinter/5servo/servo.py
+++ Firebase_GPIO_tkinter/5servo/servo.py
@@ -1,92 +1,83 @@
-from tkinter import *
-from gpiozero import AngularServo
-from HR04 import HR04
-import requests
-import json
-
+from HR04 import HR04;
+from tkinter import *;
+from gpiozero import AngularServo;
+import requests;
+import json;
+import threading
 
 class App:
-    
     def __init__(self,master):
         self.master = master;
-        self.firebase_url = "https://raspberryfirebase.firebaseio.com";
         self.angleValue = 0;
-        self.distanceValue = StringVar();
         self.valueVariable = IntVar();
-
-        #for gpio servo initial
+        self.distanceValue = StringVar();
+        self.firebase_url = "https://raspberryfirebase.firebaseio.com";
+        #gpio servo
         self.servo = AngularServo(18,min_angle=0,max_angle=90);
         self.servo.angle = 0;
-        
+
         #HR04
         self.hr04 = HR04(23,24);
         self.distanceHandler();
-        
-        
+
         #for tk
-        
         mainFrame = Frame(self.master);
         servoFrame = Frame(mainFrame);
-        borderFrame = Frame(servoFrame,borderwidth=2,relief=GROOVE,pady=30,padx=10);        
+        borderFrame = Frame(servoFrame,borderwidth=2,relief=GROOVE,pady=30,padx=10);
         Label(servoFrame,text="Servo").place(relx=0.03,anchor=NW);
-        
+
         self.button0 = Radiobutton(borderFrame,text="0",variable=self.valueVariable,value=0,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+
         self.button25 = Radiobutton(borderFrame,text="25",variable=self.valueVariable,value=25,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+
         self.button50 = Radiobutton(borderFrame,text="50",variable=self.valueVariable,value=50,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+
         self.button75 = Radiobutton(borderFrame,text="75",variable=self.valueVariable,value=75,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+
         self.button90 = Radiobutton(borderFrame,text="90",variable=self.valueVariable,value=90,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
         self.valueVariable.set(90);
-        
-        borderFrame.pack(side=TOP,padx=10,pady=10);    
-        servoFrame.pack(side=TOP);
-        
-        distanceFrame = Frame(mainFrame);
-        
-        self.label = Label(distanceFrame,textvariable=self.distanceValue);
-        self.distanceValue.set("distance:0cm");
-        self.label.pack();
-        distanceFrame.pack(side=TOP);
-        mainFrame.pack();
 
-    def __str__(self):
-        return "servo control and HR04 distance"
+        borderFrame.pack(padx=10,pady=10);
+        servoFrame.pack();
+
+        distanceFrame = Frame(mainFrame);
+        Label(distanceFrame,textvariable=self.distanceValue).pack();
+        self.distanceValue.set("distance:0cm");
+        distanceFrame.pack();
+        mainFrame.pack();
     
     def changeDegree(self):
-        self.angleValue = self.valueVariable.get();        
+        self.angleValue = self.valueVariable.get();
+        print("這值是:{}".format(self.angleValue));
         passData = {"angle":self.angleValue};
         request = requests.patch(self.firebase_url + "/raspberrypi/servo.json",data=json.dumps(passData));
 
     def distanceHandler(self):
-         try:            
+        try:
             distance = self.hr04.getCmDistance();
-            if distance != None:                
+            if distance != None:
                 self.distanceValue.set("distance:%d" % distance);
                 passData = {"distance":distance};
-                request = requests.patch(self.firebase_url + "/raspberrypi/servo.json",data=json.dumps(passData));
-            
-            getJson = requests.get(self.firebase_url + "/raspberrypi/servo.json").json();
-            angle = getJson["angle"];
-            self.valueVariable.set(angle);            
-            self.servo.angle = float(angle);
-            print(self.servo);
-         except:
+                request = requests.patch(self.firebase_url + "/raspberrypi/servo.json",data = json.dumps(passData));
+
+                #loadData
+                getJson = requests.get(self.firebase_url + "/raspberrypi/servo.json").json();
+
+                angle = getJson["angle"];
+                self.valueVariable.set(angle);
+                self.servo.angle = angle;
+        except:
             pass;
-            
-            self.master.after(1000,self.distanceHandler);
+
+        
+        threading.Timer(0.1,self.distanceHandler).start();
         
         
 
 if __name__ == "__main__":
     root = Tk();
-    root.title("servo And HC-HR04");
-    root.option_add("*font",("Helvetica",18, "bold"));
+    root.title("Servo And HC-HR04");
+    root.option_add("*font",("Helvetica",18,"bold"));
     root.option_add("*background","gold");
-    #root.option_add("*activeBackground","#333");
-    #root.option_add("*activeForeground","white");
-    #root.option_add("*highlightBackground","red");
-    #root.option_add("*HighlightColor","red");
-    app = App(root);
+    display = App(root);
     root.mainloop();
-    
-    
-    
