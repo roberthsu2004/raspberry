28d796c2e9c1fb02750a5359d77c7fdfbc3ed870
Robert Hsu <roberthsu2003@gmail.com>
2018-06-03 16:10:23 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/a1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/a1_LEDControl.py
@@ -4,72 +4,90 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import threading
-import datetime
-import time
-
 
 class App:
     
     def __init__(self,master):
-        self.ledText = StringVar();
-        self.ledState = "";
-        self.led25 = LED(25);
-        self.cred = credentials.Certificate("raspberryfirebase-firebase-adminsdk-q4ht6-1608c845ce.json");
-        firebase_admin.initialize_app(self.cred,{
+        #初始化firebase Admin database
+        cred = credentials.Certificate("raspberryfirebase-firebase-adminsdk-q4ht6-1608c845ce.json");
+        firebase_admin.initialize_app(cred,{
             'databaseURL': 'https://raspberryfirebase.firebaseio.com/'
         });
-        self.ref = db.reference("raspberrypi/LED_Control");        
+
+        #建立標題和最小大小
         master.title("LED Control");
+        master.geometry("300x200+10+10");
         master.option_add("*Font",("verdana",18,"bold"));
-        f = Frame(master);
-        xf = Frame(f,relief=GROOVE,borderwidth=2);
-        titleLabel = Label(f,text="LED Control").place(relx=0.05,rely=0.025,anchor=NW);
-        xf.pack(expand=YES,fill=BOTH,padx=5,pady=20);
-        button = Button(xf,textvariable=self.ledText,command=lambda :self.userClick()).pack(expand=YES,fill=BOTH,padx=80,pady=50);
-        f.pack(fill=BOTH,expand=YES);
-        self.getFirebaseData();
+        master.option_add("*Label.Font",("verdana",18));
+        master.option_add("*Button.Background","dark gray");
+
+        self.buttonText = StringVar();
+        self.buttonText.set("OPEN");
+
+        #led state
+        self.ledState = "";
+
+        #gpio led
+        self.led=LED(25);
+        
+        #建立最外部的Frame
+        self.mainFrame = Frame(master);
+        self.mainFrame.pack(expand=YES,fill=BOTH);
+        self.createSubFrame();
+
+        #連線資料庫，取得資料
+        self.ledControlRef = db.reference('raspberrypi/LED_Control')
+        self.getFirebaseLedControl();
     
+    def createSubFrame(self):
+        subFrame = Frame(self.mainFrame,relief=GROOVE,borderwidth=2);
+        titleLabel = Label(self.mainFrame,text="LED Control").place(relx=0.05,rely=0.025,anchor=NW);
+        self.createButton(subFrame);
+        subFrame.pack(expand=YES,fill=BOTH,padx=5,pady=20);
+    
+    
+
+    def createButton(self,subFrame):
+        button = Button(subFrame,textvariable=self.buttonText,command=self.userClick).pack(expand=YES,fill=BOTH,padx=40,pady=25);
+        
     def userClick(self):
-        if self.ledState == "OPEN" :
-            newState = "CLOSE";
+        
+        if self.ledState == "OPEN":
+            newState = "CLOSE";            
         else:
             newState = "OPEN";
-        
-        t = time.time();
-        date = datetime.datetime.fromtimestamp(t).strftime("%Y-%m-%d-%H-%M-%S");
-        print("newState={0}".format(newState));
         try:
-            self.ref.update({
-                'LED25': newState,
-                'date': date
-            });
-
+            self.ledControlRef.update({'LED25': newState});
         except:
+            print("update failure");
             return;
-        
-        
     
-    def getFirebaseData(self):
+        
+    def getFirebaseLedControl(self):
         try:
-            response = self.ref.get();            
+            data = self.ledControlRef.get();
+        except (ValueError,ApiCallError):
+            print("raise exception");
+            threading.Timer(0.2,self.getFirebaseLedControl).start();
+            return;
         except:
-            threading.Timer(0.2,self.getFirebaseData).start();
+            print("raise exception");
+            threading.Timer(0.2,self.getFirebaseLedControl).start();
             return;
         
-        self.ledState = response["LED25"];
+        self.ledState = data['LED25'];
         if self.ledState == "OPEN":
-            self.ledText.set("LED CLOSE");
-            self.led25.on();
+            self.led.on();
+            self.buttonText.set("LED CLOSE");
         elif self.ledState == "CLOSE":
-            self.ledText.set("LED OPEN");
-            self.led25.off();
+            self.led.off();
+            self.buttonText.set("LED OPEN");
+        
+        print("led25={0},date={1}".format(data['LED25'],data['date']));
+        threading.Timer(0.2,self.getFirebaseLedControl).start();
 
-        threading.Timer(0.2,self.getFirebaseData).start();
+if __name__ == "__main__":
+    root = Tk();
+    app = App(root);
+    root.mainloop();
 
-
-
-
-root = Tk();
-root.geometry("400x300");
-display = App(root);
-root.mainloop();
