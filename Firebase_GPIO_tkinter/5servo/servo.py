b69ff5912f3166b52fca5f3db003eab11cbd253b
Robert Hsu <roberthsu2003@gmail.com>
2018-07-01 16:07:43 +0800

    commit

--- Firebase_GPIO_tkinter/5servo/servo.py
+++ Firebase_GPIO_tkinter/5servo/servo.py
@@ -1,77 +1,80 @@
-from HR04 import HR04;
 from tkinter import *;
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from HR04 import HR04
 from gpiozero import AngularServo;
-import requests;
-import json;
 import threading
+
 
 class App:
     def __init__(self,master):
-        self.master = master;
-        self.angleValue = 0;
-        self.valueVariable = IntVar();
-        self.distanceValue = StringVar();
-        self.firebase_url = "https://raspberryfirebase.firebaseio.com";
-        #gpio servo
-        self.servo = AngularServo(18,min_angle=0,max_angle=90);
-        self.servo.angle = 0;
-
-        #HR04
-        self.hr04 = HR04(23,24);
-        self.distanceHandler();
-
-        #for tk
-        mainFrame = Frame(self.master);
-        servoFrame = Frame(mainFrame);
-        borderFrame = Frame(servoFrame,borderwidth=2,relief=GROOVE,pady=30,padx=10);
-        Label(servoFrame,text="Servo").place(relx=0.03,anchor=NW);
-
-        self.button0 = Radiobutton(borderFrame,text="0",variable=self.valueVariable,value=0,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
-
-        self.button25 = Radiobutton(borderFrame,text="25",variable=self.valueVariable,value=25,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
-
-        self.button50 = Radiobutton(borderFrame,text="50",variable=self.valueVariable,value=50,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
-
-        self.button75 = Radiobutton(borderFrame,text="75",variable=self.valueVariable,value=75,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
-
-        self.button90 = Radiobutton(borderFrame,text="90",variable=self.valueVariable,value=90,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
-        self.valueVariable.set(90);
-
-        borderFrame.pack(padx=10,pady=10);
-        servoFrame.pack();
-
-        distanceFrame = Frame(mainFrame);
-        Label(distanceFrame,textvariable=self.distanceValue).pack();
-        self.distanceValue.set("distance:0cm");
-        distanceFrame.pack();
-        mainFrame.pack();
+       self.master = master;
+       self.valueVariable = IntVar();
+       self.distanceValue = StringVar();
+       #for firebase
+       cred = credentials.Certificate('raspberryfirebase-firebase-adminsdk-q4ht6-1608c845ce.json');
+       firebase_admin.initialize_app(cred, {'databaseURL': 'https://raspberryfirebase.firebaseio.com/'});
+       self.servoRef = db.reference('raspberrypi/servo');
+       #for HR04
+       self.hr04 = HR04(23,24);
+       
+       #for servo
+       self.servo = AngularServo(18,min_angle=0,max_angle=90);
+       self.servo.angle = 0;
+       #for tk
+       mainFrame = Frame(self.master);
+       servoFrame = Frame(mainFrame);
+       borderFrame = Frame(servoFrame,borderwidth=2,relief=GROOVE,pady=30,padx=10);
+       Label(servoFrame,text="Servo").place(relx=0.03,anchor=NW);
+       self.button0 = Radiobutton(borderFrame,text="0",variable=self.valueVariable,value=0,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+       self.button25 = Radiobutton(borderFrame,text="25",variable=self.valueVariable,value=25,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+       self.button50 = Radiobutton(borderFrame,text="50",variable=self.valueVariable,value=50,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+       self.button75= Radiobutton(borderFrame,text="75",variable=self.valueVariable,value=75,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+       self.button90 = Radiobutton(borderFrame,text="90",variable=self.valueVariable,value=90,indicatoron=0,padx=20,command=self.changeDegree).pack(side=LEFT,padx=20);
+       borderFrame.pack(padx=10,pady=10);
+       self.valueVariable.set(75);
+       servoFrame.pack();
+       
+       #distance frame
+       distanceFrame = Frame(mainFrame);
+       Label(distanceFrame,textvariable = self.distanceValue).pack();
+       self.distanceValue.set('distance:0cm');
+       distanceFrame.pack();
+       mainFrame.pack();
+       
+       #keep doing geting data
+       self.distanceHandler();
+       
+       
+       
     
     def changeDegree(self):
         self.angleValue = self.valueVariable.get();
-        print("這值是:{}".format(self.angleValue));
-        passData = {"angle":self.angleValue};
-        request = requests.patch(self.firebase_url + "/raspberrypi/servo.json",data=json.dumps(passData));
-
+        print("this value:{0:4d}".format(self.angleValue));
+        try:
+            self.servoRef.update({
+                'angle':self.valueVariable.get()
+                });
+        except:
+            print("update angel triger Error");
+    
     def distanceHandler(self):
+        self.angleHandler();
         try:
             distance = self.hr04.getCmDistance();
             if distance != None:
-                self.distanceValue.set("distance:%d" % distance);
-                passData = {"distance":distance};
-                request = requests.patch(self.firebase_url + "/raspberrypi/servo.json",data = json.dumps(passData));
-
-                #loadData
-                getJson = requests.get(self.firebase_url + "/raspberrypi/servo.json").json();
-
-                angle = getJson["angle"];
-                self.valueVariable.set(angle);
-                self.servo.angle = angle;
+                self.distanceValue.set("distance:{0:3d}cm".format(distance));
+                self.servoRef.update({'distance':distance});
         except:
-            pass;
-
-        
-        threading.Timer(0.1,self.distanceHandler).start();
-        
+            print('get distance is Error');
+        finally:
+            threading.Timer(0.1,self.distanceHandler).start();
+    
+    def angleHandler(self):
+        servoValues = self.servoRef.get();
+        angleValue = servoValues['angle'];
+        self.servo.angle = angleValue;
         
 
 if __name__ == "__main__":
