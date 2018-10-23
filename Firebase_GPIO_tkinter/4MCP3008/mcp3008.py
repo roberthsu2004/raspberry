96ad2a4490171b02dc7c9dc061ea7ffa03e93553
Robert Hsu <roberthsu2003@gmail.com>
2018-10-23 21:15:17 +0800

    Update mcp3008.py

--- Firebase_GPIO_tkinter/4MCP3008/mcp3008.py
+++ Firebase_GPIO_tkinter/4MCP3008/mcp3008.py
@@ -1,61 +1,59 @@
 from tkinter import *
 from gpiozero import MCP3008
 from threading import Timer
-import firebase_admin;
-from firebase_admin import credentials;
-from firebase_admin import db;
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 class App:
     def __init__(self,master):
         #initial firebase
-        cred = credentials.Certificate('raspberryfirebase-firebase-adminsdk-q4ht6-1608c845ce.json');
-        firebase_admin.initialize_app(cred, {
-'databaseURL': 'https://raspberryfirebase.firebaseio.com/'});
-        self.mcp3008Ref = db.reference("raspberrypi/MCP3008");
-        
-        #initial gpiozero
-        self.ldr = MCP3008(channel=7);
-        self.adc = MCP3008(channel=6);
+        cred = credentials.Certificate('raspberryfirebase-firebase-adminsdk-q4ht6-7d3f9d2d5e.json');
+        firebase_admin.initialize_app(cred, {'databaseURL': 'https://raspberryfirebase.firebaseio.com/'})
+        self.mcp3008Ref = db.reference('raspberrypi/MCP3008')
+        #gpiozero
+        self.lightness = MCP3008(channel=7);
+        self.temperature = MCP3008(channel=6);
         
         #tkinter
-        brightnessFrame = Frame(master,relief=GROOVE,borderwidth=2);
-        brightnessSubFrame = Frame(brightnessFrame);
-        Label(brightnessSubFrame,text="brightness",anchor=NW).pack(fill=BOTH,pady=10,padx=10);
-        self.brightnessValue = StringVar();
-        Label(brightnessSubFrame,textvariable=self.brightnessValue).pack(fill=BOTH,pady=10);
-        self.brightnessValue.set("0.12345");
-        brightnessSubFrame.pack(ipady=40,ipadx=150);
-        brightnessFrame.pack(padx=30,pady=30);
+        self.temperatureText = StringVar();
+        self.lightnessText = StringVar();
         
-        temperatureFrame = Frame(master,relief=GROOVE,borderwidth=2);
-        temperatureSubFrame = Frame(temperatureFrame);
-        Label(temperatureSubFrame,text="temperature",anchor=NW).pack(fill=BOTH,pady=10,padx=10);
-        self.temperatureValue = StringVar();
-        Label(temperatureSubFrame,textvariable=self.temperatureValue).pack(fill=BOTH,pady=10);
-        self.temperatureValue.set("0.12345");
-        temperatureSubFrame.pack(ipady=40,ipadx=150);
-        temperatureFrame.pack(padx=30,pady=30);
+        mainFrame = Frame(master,relief=GROOVE, borderwidth=2);
+        Label(mainFrame, text="室內溫度:").grid(row=0,column=0,sticky=E,padx=5,pady=20);
+        Label(mainFrame, text="室內光線:").grid(row=1,column=0,sticky=E,padx=5,pady=20);
+        Entry(mainFrame, width = 16, textvariable=self.temperatureText,state = DISABLED).grid(row=0,column=1,sticky=W,padx=5,pady=20);
+        self.temperatureText.set("123.456");
+        Entry(mainFrame, width = 16, textvariable=self.lightnessText, state = DISABLED).grid(row=1,column=1,sticky=W,padx=5,pady=20);
+        self.lightnessText.set("456.789");
+        mainFrame.pack(padx=10,pady=10);
+        
         self.autoUpdate();
         
     def autoUpdate(self):
-        self.brightnessValue.set("brightness:{0:.2f}".format(self.ldr.value * 1000));
-        self.temperatureValue.set("temperature:{0:.2f}".format(self.adc.value * 3.3 * 100));
+        lightnessValue = self.lightness.value * 1000;
+        temperatureValue = self.temperature.value * 3.3 * 100;
+        self.temperatureText.set("{:.2f}".format(temperatureValue));
+        self.lightnessText.set("{:.2f}".format(lightnessValue));
         try:
             self.mcp3008Ref.update({
-                'brightness':self.ldr.value * 1000,
-                'temperature':self.adc.value * 3.3 * 100
+                'brightness':lightnessValue,
+                'temperature':temperatureValue
                 });
         except:
             pass;
         
+        
         Timer(1,self.autoUpdate).start();
         
 
-
 if __name__ == "__main__":
-    root = Tk();
-    root.title("溫度和光線感應");
-    root.option_add("*font",("verdana",18,"bold"));
-    root.option_add("*background","gold");
-    root.option_add("*foreground","#888888");
-    display = App(root);
-    root.mainloop();
+    window = Tk();
+    window.title("溫度和光線的感應");
+    window.option_add("*font",("verdana",18,"bold"));
+    window.option_add("*background", "#068587");
+    window.option_add("*foreground", "#FFFFFF");
+    #window.geometry("300x200");
+    display = App(window);
+    window.mainloop();
+    
