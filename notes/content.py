046df56f981b9aec30cc0fdacefce55393066713
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 16:01:52 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -1,9 +1,12 @@
 while True:
     print("請計算圓面積");
     r = input("請輸入半徑:");
+    if r == "R":
+        break;
     floatR = float(r);
     area = 3.1415926 * floatR ** 2;
     print("圓面積為:" + str(area));
     print("\n\n\n");
     print("=========================================");
 
+print("bye bye")
