b03171b1edf78157ff8765851ee4858bd9549873
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 14:33:44 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -1,23 +1,9 @@
-bottles = 99
-base = '';
-base += 'current inventory';
-base += str(bottles);
-print(base);
-
-print(str(98.6));
-print(str(1.0e4));
-print(str(True));
-
-#'\'
-
-palindrome = 'A ma\'n,\nA plan, \nA canal; \npan\tama.';
-print(palindrome);
-
-
 letters = 'abcdefghijklmnopqrstuvwxyz';
 print(letters[0]);
 print(letters[-1]);
 #print(letters[100]);
-letters[0] = 'c'
+#letters[0] = 'c'
 
-
+name = 'Henny';
+name = name.replace('H','P')
+print(name);
