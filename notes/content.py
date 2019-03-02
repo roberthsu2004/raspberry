e528d9c225aa7bcdbdd401568d68af7fb3f1c292
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 13:48:45 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -1,5 +1,15 @@
-print(type(True));
-print(float(True));
-print(float(False));
-print(int('1234'));
-print(int(123.56768));
+bottles = 99
+base = '';
+base += 'current inventory';
+base += str(bottles);
+print(base);
+
+print(str(98.6));
+print(str(1.0e4));
+print(str(True));
+
+#'\'
+
+palindrome = 'A ma\'n,\nA plan, \nA canal; \npan\tama.';
+print(palindrome);
+
