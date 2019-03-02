d7e91ef714d6adffa12f9525c1140abc36da0cfb
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 14:56:07 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -14,3 +14,9 @@ print(letters[10:]);
 print(letters[12:15]);
 print(letters[-3:]);
 print(letters[4:20:3]);
+
+print(len(letters))
+todos = 'get gloves, get mask, give cat vitamins, call ambulance'
+dos = todos.split(',');
+print(dos);
+print(todos.find('cat'));
