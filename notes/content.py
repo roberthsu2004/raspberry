f5d12292e13a7d35138002e9ca0d307e96a19478
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 14:06:08 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -13,3 +13,11 @@ print(str(True));
 palindrome = 'A ma\'n,\nA plan, \nA canal; \npan\tama.';
 print(palindrome);
 
+
+letters = 'abcdefghijklmnopqrstuvwxyz';
+print(letters[0]);
+print(letters[-1]);
+#print(letters[100]);
+letters[0] = 'c'
+
+
