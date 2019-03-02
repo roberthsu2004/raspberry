fa1be36ab782945b14dcea22d7e3762aad6ebf6e
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 12:28:02 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -1,32 +1,5 @@
-a = 6*7
-print(a);
-a = 6 * 7 * 2 * 3
-print(a)
-a = 9 / 5;
-print(a);
-a = 9 // 5;
-print(a);
-#a = 5 / 0;
-a = 95;
-a = a - 3;
-
-a = 95;
-a -= 3;
-print(a);
-print(99 ** 10000);
-
-print(9 % 5);
-
-print( (2 + 3) * 4);
-print('snap');
-print("snap");
-
-print("Nay, said the naysayer's ");
-print('Nay, said the naysayer"s ');
-
-s1 = """舉辦在屏東的台灣燈會，光是228一天就湧入了167萬人潮，而1日更是刷新紀錄達到169萬，總人次突破1000萬人，而這也讓屏東網友紛紛大讚：「人進來了啊！」此外，屏東縣長潘孟安也在臉書感性發文感謝工作人員的努力，因此又讓不少網友感動淚嘆：「我屏東我驕傲」，但也有另一部分民眾感嘆，「高雄看不到屏東的車尾燈了！」。
-原文網址: 影／台灣燈會破1000萬人次！團隊檢討到凌晨累壞　網：高雄看不到屏東車尾燈 | ETtoday社會 | ETtoday新聞雲 https://www.ettoday.net/news/20190302/1389799.htm#ixzz5gz60yuUO 
-Follow us: @ETtodaynet on Twitter | ETtoday on Facebook"""
-print(s1);
-
-
+print(type(True));
+print(float(True));
+print(float(False));
+print(int('1234'));
+print(int(123.56768));
