116b025241ba64b992b11737189f07b1f4fe3048
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-03-02 11:31:53 +0800

    Update content.py

--- notes/content.py
+++ notes/content.py
@@ -1,5 +1,8 @@
-a = 7
+a = 7.52345678903
+print(type(a))
 b = a
+a = 5;
+print(type(a))
 print("a=",a)
 print("b=",b)
 
@@ -8,4 +11,7 @@ _a = 12375688979879080989080980989077868;
 _b = 567345465476575687687897980980989089;
 total = _a * _b;
 print("total=",total);
+print("_a",type(_a));
+print(type("hello! world!"))
+
 
