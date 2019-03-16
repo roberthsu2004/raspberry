e75a0b68c4e62fb81fb4176ceefb9d5263732419
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-16 11:01:30 +0800

    modify

--- python/list.py
+++ python/list.py
@@ -4,3 +4,23 @@ big_birds = ['emu', 'ostrich', 'cassowary']
 first_names = ['Graham', 'John', 'Jerry', 'Terry', 'Terry', 'Michael']
 print(weekdays)
 print(type(weekdays))
+
+another_empty_list = list()
+print(another_empty_list)
+
+cat_list = list('cat')
+print(cat_list)
+
+a_tuple = ('ready', 'fire', 'aim')
+print(a_tuple)
+print(type(a_tuple))
+
+birthday = '1/3/1978'
+birthday_list = birthday.split('/')
+print(birthday_list)
+
+marxes = ['Groucho', 'Chico', 'Harpo']
+print(marxes[0])
+print(marxes[1])
+print(marxes[2])
+print(marxes[3])
