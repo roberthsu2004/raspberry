bfc38fe63b4c3341d699cf5d252c970a3bc55c93
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-16 10:40:49 +0800

    modify

--- python/list.py
+++ python/list.py
@@ -1,1 +1,6 @@
 empty_list = []
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+big_birds = ['emu', 'ostrich', 'cassowary']
+first_names = ['Graham', 'John', 'Jerry', 'Terry', 'Terry', 'Michael']
+print(weekdays)
+print(type(weekdays))
