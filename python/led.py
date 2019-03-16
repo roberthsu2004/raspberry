aad7ee0043b9e47ac3e51c86f142d44d41a9f3ba
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-16 13:52:25 +0800

    modify

--- python/led.py
+++ python/led.py
@@ -1,1 +1,14 @@
 import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25,GPIO.OUT)
+GPIO.setwarnings(False)
+
+while True:
+    GPIO.output(25,GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(25,GPIO.LOW)
+    time.sleep(0.2)
+
+GPIO.cleanup();
