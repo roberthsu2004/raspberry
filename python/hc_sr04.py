d32086dcba6fdd9e9fdb5032be3329b8c68d9a67
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-23 16:04:57 +0800

    modify

--- python/hc_sr04.py
+++ python/hc_sr04.py
@@ -12,7 +12,10 @@ try:
         raw_measurement = sr04.raw_distance()
         distance = sr04.distance_metric(raw_measurement)
         print(distance)
-        time.sleep(1)
+        if distance <= 20:
+            print("light on")
+        else:
+            print("light off")
 except KeyboardInterrupt:
     print("control-c 關閉程式")
 except:
