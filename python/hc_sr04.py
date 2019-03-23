378e17dee690004e03096905345618e356fe1310
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-23 14:59:31 +0800

    modify

--- python/hc_sr04.py
+++ python/hc_sr04.py
@@ -7,7 +7,12 @@ print("按下STOP可以中斷程式")
 try:
     while True:
         print("run")
-        time.sleep(10)
+        
+        sr04 = sensor.Measurement(TRIGGER_PIN, ECHO_PIN)
+        raw_measurement = sr04.raw_distance()
+        distance = sr04.distance_metric(raw_measurement)
+        print(distance)
+        time.sleep(1)
 except KeyboardInterrupt:
     print("control-c 關閉程式")
 except:
