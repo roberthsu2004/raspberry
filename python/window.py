1aff6a7fe37aea05f6461c739ed4d5bced9353f7
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-30 14:06:51 +0800

    modify

--- python/window.py
+++ python/window.py
@@ -2,12 +2,19 @@ from tkinter import *
 
 master = Tk()
 
-def callback():
-    print("click!")
+def openLed():
+    print("Open")
 
-b = Button(master, text="OK", command=callback)
-b.pack()
+def closeLed():
+    print("Close")
 
-mainloop()
+
+openBtn = Button(master, text="OPEN", command=openLed)
+openBtn.pack()
+for i in range(1,10):
+    closeBtn = Button(master, text=("CLOSE"+str(i)),command=closeLed)
+    closeBtn.pack()
+
+master.mainloop()
 
 #http://effbot.org/tkinterbook/button.htm
