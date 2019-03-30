0738a974fa2578c957f5ef9a4b18572e2cbefc04
roberthsu2004 <roberthsu2004@gmail.com>
2019-03-30 10:28:56 +0800

    modify

---
+++ python/set.py
@@ -0,0 +1,8 @@
+empty_set = set()
+print(empty_set)
+even_numbers = {0, 2, 4, 6, 8}
+print(even_numbers)
+old_numbers = {1, 3, 5, 7, 9, 1, 3}
+old_lists = list(old_numbers)
+old_lists.sort()
+print(old_lists)
