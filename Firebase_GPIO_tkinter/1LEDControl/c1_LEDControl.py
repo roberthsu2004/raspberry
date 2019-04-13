cbd7f4e90657840ea91c76385fbe43c0120cd247
roberthsu2004 <roberthsu2004@gmail.com>
2019-04-13 15:20:07 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -1,15 +1,22 @@
 from tkinter import *
 from sys import path
 
+
+
+
 def displayWindow(w):
     buttonText = StringVar()
     buttonText.set("OPEN")
     mainFrame = Frame(w,borderwidth=2,relief=GROOVE)    
-    Button(mainFrame,textvariable=buttonText,font=("Helvetica", 18, "bold italic")).pack(expand=True,fill=BOTH,padx=40,pady=25);
+    Button(mainFrame,textvariable=buttonText,font=("Helvetica", 18, "bold italic"),command=userClick).pack(expand=True,fill=BOTH,padx=40,pady=25);
     mainFrame.pack(expand=True,fill=BOTH,padx=10,pady=10)
-    buttonText.set("close")
+   
+def userClick():    
+    print("user click");
+    print(buttonText.get())
 
 if __name__ == "__main__":
+    
     window = Tk();
     window.title("LED Control")
     window.geometry("300x200")
