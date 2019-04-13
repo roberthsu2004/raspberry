c1bd0a65775429b4ef85bbcefa6c32512289e4fc
roberthsu2004 <roberthsu2004@gmail.com>
2019-04-13 12:32:12 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -1,7 +1,16 @@
 from tkinter import *
+from sys import path
+
+def displayWindow(w):
+    print("displayWindow");
+    print(type(w))
+    pass;
 
 if __name__ == "__main__":
     window = Tk();
-    
+    window.title("LED Control")
+    window.geometry("300x200")
+    displayWindow(window);
+   
     window.mainloop();
     
