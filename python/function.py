ee238c8f9847cf4c59c8b2f6871ec62dc4bb5f5b
roberthsu2004 <roberthsu2004@gmail.com>
2019-04-13 10:38:34 +0800

    modify

--- python/function.py
+++ python/function.py
@@ -4,6 +4,22 @@ def do_nothing():
 def echo(anything):
     return anything + ' ' + anything
 
+def commentary(color):
+    if color == 'red':
+        return "It's a tomato"
+    elif color == "green":
+        return "It's a green pepper."
+    elif color == "bee purple":
+        return " I don't know what"
+    else:
+        return "I've never heard of the color " + color + "."
+    
+def menu(wine, entree, dessert):
+    return {'wine':wine, 'entree':entree, 'dessert':dessert}
+
+    
 do_nothing();
 print(echo('Rumplestiltskin'))
+print(commentary("vilot"));
+print(menu('chardonnay','chicken','cake'));
 
