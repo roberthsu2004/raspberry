4954a23c7f8ba16ea389ddd59fe6424d9ee8f0fc
roberthsu2004 <roberthsu2004@gmail.com>
2019-04-13 14:21:55 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -3,7 +3,8 @@ from sys import path
 
 def displayWindow(w):
     mainFrame = Frame(w,borderwidth=2,relief=GROOVE)
-    mainFrame.pack(padx=10,pady=10)
+    Button(mainFrame,text="Press Me").pack(expand=True,fill=BOTH,padx=40,pady=25);
+    mainFrame.pack(expand=True,fill=BOTH,padx=10,pady=10)
 
 if __name__ == "__main__":
     window = Tk();
