e6d02b160adf985229661dad957a1c0e45be2784
roberthsu <roberthsu2003@gmail.com>
2019-04-20 16:28:02 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from gpiozero import LED as LightBolt
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 buttonText = None
 lightBolt = LightBolt(4)
