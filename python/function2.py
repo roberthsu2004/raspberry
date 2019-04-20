0df8341a3998e32d5626dd7c99af71dd85e7aea4
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-04-20 10:32:54 +0800

    Update function2.py

--- python/function2.py
+++ python/function2.py
@@ -11,3 +11,17 @@ def print_more(required1,required2,*args):
     print('all the rest:',args)
 
 print_more('cap', 'gloves', 'scarf', 'monocle', 'mustache wax')
+
+
+def print_kwargs(**kwargs):
+    print('keyword arguments:', kwargs)
+
+print_kwargs(wine='merlot', entree='mutton', dessert='macaroon');
+
+#docsString
+
+def echo(anything):
+    ' echo returns its input argument'
+    return anything
+
+print(echo.__doc__)
