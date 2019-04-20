4f2b363bbb906d47e7e59379c82bc12162c36d1c
roberthsu2004 <45559570+roberthsu2004@users.noreply.github.com>
2019-04-20 11:08:53 +0800

    Update function3.py

--- python/function3.py
+++ python/function3.py
@@ -1,1 +1,13 @@
-hello!!
+def edit_story(words, func):
+    for word in words:
+        print(func(word))
+        
+#stairs = ['thud', 'meow', 'thud', 'hiss']
+
+#def enliven(word):
+#   return word.capitalize() + '!'
+
+
+#edit_story(stairs,enliven)
+
+edit_story(['thud', 'meow', 'thud', 'hiss'],lambda word : word.capitalize()+'!')
