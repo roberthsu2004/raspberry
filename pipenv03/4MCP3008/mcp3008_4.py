08c163b21e3f3b63d109d0eb1025e029fe57019f
roberthsu <roberthsu2003@gmail.com>
2019-05-11 10:29:24 +0800

    modify

---
+++ pipenv03/4MCP3008/mcp3008_4.py
@@ -0,0 +1,10 @@
+from tkinter import *
+if __name__ == "__main__":
+    window = Tk()
+    window.title("溫度和光線的感應")
+    window.option_add("*font",("verdana",18,"bold"))
+    window.option_add("*background","#068587")
+    window.option_add("*foreground", "#FFFFFF")
+    window.geometry("500x200")
+    window.mainloop()
+    
