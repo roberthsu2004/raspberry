13ba900d154dd1c803a348eba565cf37774f74dd
roberthsu <roberthsu2003@gmail.com>
2019-05-11 14:19:18 +0800

    modify

--- pipenv03/4MCP3008/mcp3008_4.py
+++ pipenv03/4MCP3008/mcp3008_4.py
@@ -7,25 +7,23 @@ class GUI:
         self.lightness = MCP3008(channel=7)
         self.temperature = MCP3008(channel=6)
         
-        temperatureText = StringVar()
-        lightnessText = StringVar();
+        self.temperatureText = StringVar()
+        self.lightnessText = StringVar();
         mainFrame = Frame(w,relief=GROOVE, borderwidth=2)
         Label(mainFrame, text="溫度").grid(row=0, column=0,sticky=E,padx=5,pady=20)
         Label(mainFrame, text="光線").grid(row=1, column=0,sticky=E,padx=5,pady=20)
-        Entry(mainFrame, width= 25, state= DISABLED, textvariable=temperatureText).grid(row=0,column=1,sticky=W,padx=5,pady=20)
-        temperatureText.set("123.456")
-        Entry(mainFrame, width= 25, state= DISABLED, textvariable=lightnessText).grid(row=1,column=1,sticky=W,padx=5,pady=20)
-        lightnessText.set("456.789")
+        Entry(mainFrame, width= 25, state= DISABLED, textvariable=self.temperatureText).grid(row=0,column=1,sticky=W,padx=5,pady=20)
+        self.temperatureText.set("123.456")
+        Entry(mainFrame, width= 25, state= DISABLED, textvariable=self.lightnessText).grid(row=1,column=1,sticky=W,padx=5,pady=20)
+        self.lightnessText.set("456.789")
         mainFrame.pack(padx=10,pady=10)
         self.autoUpdate()
     
-    def autoUpdate(self):
-       
-        
+    def autoUpdate(self):        
         lightnessValue = self.lightness.value * 1000;
         temperatureValue = self.temperature.value * 3.3 * 100
-        print(lightnessValue)
-        print(temperatureValue)
+        self.temperatureText.set("{:.2f}".format(temperatureValue))
+        self.lightnessText.set("{:.3f}".format(lightnessValue))
         Timer(1,self.autoUpdate).start()
 
 if __name__ == "__main__":
