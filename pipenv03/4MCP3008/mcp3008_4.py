c50f31f44a63fb6ae921ca92afcaaca584acc0e9
roberthsu <roberthsu2003@gmail.com>
2019-05-11 11:44:00 +0800

    modify

--- pipenv03/4MCP3008/mcp3008_4.py
+++ pipenv03/4MCP3008/mcp3008_4.py
@@ -2,12 +2,16 @@ from tkinter import *
 
 class GUI:
     def __init__(self,w):
+        temperatureText = StringVar()
+        lightnessText = StringVar();
         mainFrame = Frame(w,relief=GROOVE, borderwidth=2)
         Label(mainFrame, text="溫度").grid(row=0, column=0,sticky=E,padx=5,pady=20)
         Label(mainFrame, text="光線").grid(row=1, column=0,sticky=E,padx=5,pady=20)
-        Entry(mainFrame, width= 25, state= DISABLED, text='temperature').grid(row=0,column=1,sticky=W,padx=5,pady=20)
-        Entry(mainFrame, width= 25, state= DISABLED, text='sunshin').grid(row=1,column=1,sticky=W,padx=5,pady=20)
-        mainFrame.pack()
+        Entry(mainFrame, width= 25, state= DISABLED, textvariable=temperatureText).grid(row=0,column=1,sticky=W,padx=5,pady=20)
+        temperatureText.set("123.456")
+        Entry(mainFrame, width= 25, state= DISABLED, textvariable=lightnessText).grid(row=1,column=1,sticky=W,padx=5,pady=20)
+        lightnessText.set("456.789")
+        mainFrame.pack(padx=10,pady=10)
 
 if __name__ == "__main__":
     window = Tk()
